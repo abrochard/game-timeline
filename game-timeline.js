c2de330a5ccda50b6c6e1ecd855542e456c82ddb
abrochard <aaa.brochard@gmail.com>
2016-11-30 00:10:38 -0500

    updated images

--- game-timeline.js
+++ game-timeline.js
@@ -1,11 +1,43 @@
-angular.module('gameApp', ['angular-timeline', 'angular-scroll-animate'])
+angular.module('gameApp',
+               [
+                 'angular-timeline',
+                 'angular-scroll-animate'
+               ])
   .controller('GameTimelineController', function($scope) {
 
     $scope.games = games;
+    $scope.backup = clone(games);
+    $scope.filters = {};
+
+    function clone(obj) {
+      var clone = [];
+      for (var i = 0; i < obj.length; i++) {
+        clone[i] = obj[i];
+      }
+      return clone;
+    }
 
     $scope.reorder = function() {
-      self.games.sort(function(a, b) {
-        return a.title <= b.title;
-      });
+      if ($scope.filters.alphabetical == true) {
+        $scope.games = clone($scope.backup);
+        $scope.filters.alphabetical = false;
+      } else {
+        $scope.games.sort(function(a, b) {
+          return a.title > b.title;
+        });
+        $scope.filters.alphabetical = true;
+      }
+    };
+
+    $scope.completedOnly = function() {
+      if ($scope.filters.completedOnly == true) {
+        $scope.games = $scope.backup;
+        $scope.filters.completedOnly = false;
+      } else {
+        $scope.games = $scope.games.filter(function(g) {
+          return g.completed == true;
+        });
+        $scope.filters.completedOnly = true;
+      }
     };
   });
